470db5de2b66201ce49622a6ef8e437378963a75
sealione1311 <sealione@gmail.com>
2020-04-22 14:13:15 +0300

    исправила ошибки

--- src/components/trip-point.js
+++ src/components/trip-point.js
@@ -21,7 +21,7 @@ export const createTripPoint = ({type, destination, eventPrice, startDate, endDa
     `<li class="trip-events__item">
       <div class="event">
         <div class="event__type">
-          <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
+          <img class="event__type-icon" width="42" height="42" src="img/icons/${type.toLowerCase()}.png" alt="Event type icon">
         </div>
         <h3 class="event__title">${type} ${action} ${destination}</h3>
 
